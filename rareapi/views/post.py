01dfd097d7b20b458fac5ea2c741cff838d25f49
Ollie Lichtenstein <oliverlichtenstein1@gmail.com>
2022-05-18 14:02:31 -0500

    updated viewset for posts to display list correctly

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework import serializers, status
 from django.core.exceptions import ValidationError
 from rareapi.models import Post
+from rareapi.models import RareUser
+from django.contrib.auth.models import User
 
 
 # MAKE SURE THERE IS A POST VIEWSET IN THE rareserver/urls.py
@@ -27,24 +29,36 @@ class PostView(ViewSet):
     def list(self, request):
         """Handle GET requests to get all posts"""
         
-        posts = Post.objects.all()
-        
+        posts = Post.objects.filter(approved=True).order_by('publication_date')
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
 
         """Returns:
             Response -- JSON serialized list of posts
         """
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name')
+         
+
         
+class RareUserSerializer(serializers.ModelSerializer):
+    user= UserSerializer() 
+    class Meta:
+        model = RareUser
+        fields = ('id', 'bio', 'user')
+              
         
 class PostSerializer(serializers.ModelSerializer):
     """JSON serializer for posts
     """
+    user= RareUserSerializer()
     class Meta:
         model = Post
         fields = ('id', 'title', 'publication_date', 'image_url', 'content', 'approved', 
                   'category', 'user')
-        depth = 1      
+        depth = 2      
         
         
         
